520ffcc58c8b7492bde221b6e045f21d5b890c77
Subhampreet Mohanty <subhampreet18@gmail.com>
2021-09-25 10:01:21 +0000

    UI Improvement SearchBar Banner

--- src/components/Emergency/sub-components/SearchBar.js
+++ src/components/Emergency/sub-components/SearchBar.js
@@ -8,25 +8,44 @@ import search from '../../../assets/img/Group 7289.png';
 function SearchBar() {
     return (
         <div className="SearchBar">
-            Hello
-            {/* <header>
-            <div className = "head-text">
-                <a className="banner" href="#">
-                            <img src = { banner } alt = { banner } style = {{ width: '100%' }}/>
-                </a>
-                <div>
-                <h1 class="text-on-image" gutterBottom variant="h5" component="h5"> Help us Redefine Emergency aid<br/>and healthcare, with RAH
-                
-                </Typography>
+            <img src={banner} alt = { banner } style = {{ width: '100%' }} />
+            <diV className="body">
+                <h1> Help us Redefine Emergency aid<br/>and healthcare, with RAH</h1>
+
+                <div className="search-body">
+                    <div className="search-item">
+                        <div className="left">
+                            <h3>Your Location</h3>
+                            <textarea>Search your area</textarea>
+                        </div>
+                        <div className="right"></div>
+                    </div>
+
+                    <div className="search-item">
+                        <div className="left">
+                            <h3>Your Location</h3>
+                            <textarea>Search your area</textarea>
+                        </div>
+                        <div className="right"></div>
+                    </div>
+
+                    <div className="search-item">
+                        <div className="left">
+                            <h3>Your Location</h3>
+                            <textarea>Search your area</textarea>
+                        </div>
+                        <div className="right"></div>
+                    </div>
+
+                    <div className="search-button">
+                        <h4>Search</h4>
+                        <img />
+                    </div>
                 </div>
 
-                <a class="search" href="#">
-                            <img src = { search } alt = { search } style = {{ width: '100%',top: '100px',left:'30px' }}/>
-                </a>
-            </div>
-            </header> */}
 
-
+                <h2>Get Ambulance according to your preferred location</h2>
+            </diV>
         </div>
     )
 }
